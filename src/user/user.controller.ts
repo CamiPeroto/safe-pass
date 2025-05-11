93209a9eb63cabe938aacd22aea844ab72ed25b6
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-11 19:41:23 -0300

    feat: instalando o bcrypt para criptografar as senhas de usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { Prisma, User as UserModel} from '@prisma/client';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
   @Post('signup')
   async signupUser(
@@ -13,4 +23,35 @@ export class UserController {
     return this.userService.createUser(userData);
   }
 
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    const userId = Number(id);
+    if (isNaN(userId)) {
+      throw new BadRequestException('Invalid user ID');
+    }
+
+    const user = await this.userService.user({ id: userId });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    return user;
+  }
+
+  @Put()
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser (@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
 }
