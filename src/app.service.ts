de56eeaab923d80f295dc7335354e70c4f579b2c
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-18 17:35:22 -0300

    fix: ajustando a rota get

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Conexão realizada com sucesso, tome o Hello World!';
   }
 }
