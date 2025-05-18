fd30cb585973df56d034c28d3ee6f0031a802655
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-18 17:32:58 -0300

    feat: habilitando o cors na comunicação com o front

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ app.enableCors({
+    origin: 'http://localhost:3000', // endereço do frontend
+    credentials: true, // permite envio de cookies e headers como Authorization
+  });
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
+
+
